6e24f9896de51d4c45d33b79687cb4adc296a915
jasonmrcllino <jason.marcellino.1@gmail.com>
2026-01-18 10:51:48 +0700

    enable cors vercel

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,6 +4,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true, // authorize all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Simple Storage dApp API')
